fe2caaca44322b1d710e10d2d7e7d3dba8a9ac78
Chonsawat <chonsawat.nakanam@kkumail.com>
2022-01-15 01:04:15 +0700

    Card templete & Fetch

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+App = {
+  programmingList: [
+    { id: 0, text: 'Python' },
+    { id: 1, text: 'Java Script' },
+    { id: 2, text: 'Java' },
+    { id: 3, text: 'C' },
+    { id: 3, text: 'C++' },
+  ],
+
+  contact: {
+    github: "https://github.com/chonsawat",
+    linkedIn: "https://www.linkedin.com/in/chonsawat-nakanam-0a09241b0",
+    email: "chonsawat.nakanam@kkumail.com",
+    facebook: "https://www.facebook.com/chonsawt.naknam",
+    instagram: "https://www.instagram.com/cmmbit/",
+    twitter: "https://twitter.com/CNakanam"
+  },
+
+  contents: [
+    {
+      title: "ChestXray",
+      subTitle: "Deep Neural Network, Multi-label Classification",
+      link: "https://colab.research.google.com/drive/1wKeS6WROIFh5lFDp1QRJsYe47S3aEBtg?usp=sharing",
+      text: "โปรเจ็ครายวิชา Research Methodology เกี่ยวกับการจำแนกประเภทแบบหลายป้ายด้วยภาพรังสีทรวงอก",
+      img: "https://images.unsplash.com/photo-1631563019701-efcf403bc5fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      title: "Computer Network",
+      subTitle: "-",
+      link: "https://github.com/chonsawat/Computer-Network",
+      text: "",
+      img: "https://images.unsplash.com/photo-1562408590-e32931084e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      title: "Data Engineer",
+      subTitle: "-",
+      link: "https://github.com/chonsawat/Data-Engineer",
+      text: "",
+      img: "https://images.unsplash.com/photo-1634942536846-e9863ef9e78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+    },
+    {
+      title: "Mini Drink Shop",
+      subTitle: "-",
+      link: "https://github.com/chonsawat/MiniDrinkShop",
+      text: "",
+      img: "https://images.unsplash.com/photo-1625726124021-feb7ec3f281b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+    },
+  ]
+}
+
 new Vue({
   el: '#app',
   vuetify: new Vuetify(),
@@ -7,21 +57,10 @@ new Vue({
         title: "Chonsawat's Portfolio",
         about: "สวัสดีครับ😊 ผมชนม์สวัสดิ์ นาคนาม นักศึกษาวิทยาการคอมพิวเตอร์ วิทยาลัยการคอมพิวเตอร์ มหาวิทยาลัยขอนแก่นครับ",
         description: "",
-        contact: {
-          github: "https://github.com/chonsawat",
-          linkedIn: "https://www.linkedin.com/in/chonsawat-nakanam-0a09241b0",
-          email: "chonsawat.nakanam@kkumail.com",
-          facebook: "https://www.facebook.com/chonsawt.naknam",
-          instagram: "https://www.instagram.com/cmmbit/",
-          twitter: "https://twitter.com/CNakanam"
-        },
-        programmingList: [
-          { id: 0, text: 'Python' },
-          { id: 1, text: 'Java Script' },
-          { id: 2, text: 'Java' },
-          { id: 3, text: 'C' },
-        ],
+        contact: App.contact,
+        programmingList: App.programmingList,
         cardShow: false,
+        projectCards: App.contents,
       }
   },
 })
--- templete.js
+++ templete.js
@@ -2,3 +2,41 @@ Vue.component('todo-item', {
     props: ['todo'],
     template: /*html*/`<li>{{ todo.text }}</li>`
 })
+
+var data = {
+    title: "ChestXray",
+    subTitle: "Deep Neural Network, Multi-label Classification",
+    link: "https://colab.research.google.com/drive/1wKeS6WROIFh5lFDp1QRJsYe47S3aEBtg?usp=sharing",
+    text: "โปรเจ็ครายวิชา Research Methodology เกี่ยวกับการจำแนกประเภทแบบหลายป้ายด้วยภาพรังสีทรวงอก"
+};
+
+Vue.component('project-card', {
+    props: ['data'],
+    data: function () {
+        return {
+            cardShow: false
+        }
+    },
+    template: /*html*/`
+    <v-card max-width="350">
+        <v-img :src="data.img" height="200px"></v-img>
+        <v-card-title>{{data.title}}</v-card-title>
+        <v-card-subtitle>{{data.subTitle}}</v-card-subtitle>
+        <v-card-actions>
+            <a :href="data.link" target="_blank"> <v-btn text dark color="pink"> 🔍 EXplore </v-btn> </a>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="cardShow = !cardShow">
+                <v-icon>{{ cardShow ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="cardShow">
+                <v-divider></v-divider>
+                <v-card-text>
+                {{data.text}}
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
+    `
+})
